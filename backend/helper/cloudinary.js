03567402e40f1bd2a0e9a79addb7ce7324bfa8da
Tushar-cm <tusharpandeyrocks@gmail.com>
2024-08-28 12:03:54 +0530

    Initial commit

---
+++ backend/helper/cloudinary.js
@@ -0,0 +1,8 @@
+const cloudinary = require('cloudinary');
+
+cloudinary.v2.config({
+  cloud_name: 'dw7y72phv',
+  api_key: '777293517644847',
+  api_secret: 'Vj-ncgiQEdInQMJETFZ2Bdtxwc8',
+  secure: true,
+});
